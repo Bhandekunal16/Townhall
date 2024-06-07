d4e8b5ea17583a5fec5124d35eaf65bd1d540370
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-07 14:34:46 +0530

    "Updated Logger import path from 'robotic.js/interface/Logger' to 'robotic.js/src/interface/Logger'"

--- module/index.js
+++ module/index.js
@@ -1,5 +1,5 @@
 const readline = require("readline");
-const Logger = require("robotic.js/interface/Logger");
+const Logger = require("robotic.js/src/interface/Logger");
 const TypeScript = require("./typeScript");
 const JavaScript = require("./javaScript");
 const Binary = require("./bin");
