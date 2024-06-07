c5f8c04870beaa183cb7032dba37208d98f30e10
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-07 16:29:08 +0530

    "Remove app.module.js and user.module.js files"

--- app.module.js
+++
@@ -1,6 +0,0 @@
-const service = require("../user.module");
-const App = service;
-console.log(App);
-
-module.exports = App;
-
--- user.module.js
+++
@@ -1,7 +0,0 @@
-const AccountService = require("account");
-const PersonService = require("../person");
-
-const account = new AccountService(generateUUID());
-const service = [PersonService, AccountService];
-module.exports = { service };
-console.log(module.exports);
