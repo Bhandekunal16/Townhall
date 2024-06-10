de23e193355a9080db1fba870045f4802bfef4a0
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-10 13:44:28 +0530

    "Added TFJS Log import and refactored Logger.log calls to separate lines"

--- exteroceptor.js
+++ exteroceptor.js
@@ -1,3 +1,5 @@
+const { Log } = require("@tensorflow/tfjs");
+
 const [db, Logger] = [
   require("roboticdb/src/brain"),
   require("robotic.js/src/interface/Logger"),
@@ -15,12 +17,12 @@ module.exports = function isLogger(req, res, next) {
           },
           "log"
         )
-      : new Logger().log({
-          url: req.url,
-          method: req.method,
-          host: req.hostname,
-          body: req.body,
-          hitOn: `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
-        })
+      : new Logger().log(req.url),
+    new Logger().log(req.method),
+    new Logger().log(req.hostname),
+    new Logger().log(req.body),
+    new Logger().log(
+      `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`
+    )
   );
 };
