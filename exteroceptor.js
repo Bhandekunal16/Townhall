bf349842aa350c1823cf5680d381b6e08bb8e6b8
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-10 13:24:23 +0530

    "Added 'hitOn' field with current date to log data in isLogger function"

--- exteroceptor.js
+++ exteroceptor.js
@@ -1,9 +1,14 @@
 const db = require("roboticdb/src/brain");
 module.exports = function isLogger(req, res, next) {
-  console.log(req);
   next(
     new db().write(
-      { url: req.url, method: req.method, host: req.host, body: req.body },
+      {
+        url: req.url,
+        method: req.method,
+        host: req.host,
+        body: req.body,
+        hitOn: `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
+      },
       "log"
     )
   );
