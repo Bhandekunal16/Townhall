8b252387d4afacfa40af2af59c3de1e4299853e5
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-10 12:20:48 +0530

    "Updated .gitignore file and modified type definitions in authorized.d.ts"

--- types/authorized.d.ts
+++ types/authorized.d.ts
@@ -23,7 +23,49 @@ declare class Authorized {
   createExcelFile(dataArray: any[], filePath: string): Promise<void>;
   readExcelToArray(filePath: string, output: any): Promise<any>;
   getPackageInfo(packageName: string): Promise<any>;
-  NpmSearch(name: string): Promise<any>;
-  NpmView(userName: string, packageName: string): Promise<any>;
-  NpmViewV2(name: string): Promise<any>;
+  NpmSearch(name: string): Promise<
+    | {
+        data: any;
+        statusCode: number;
+        status: boolean;
+        msg: any;
+      }
+    | {
+        response: any;
+        statusCode: number;
+        status: boolean;
+        msg: any;
+      }
+  >;
+  NpmView(
+    userName: string,
+    packageName: string
+  ): Promise<
+    | {
+        data: any;
+        statusCode: number;
+        status: boolean;
+        msg: any;
+      }
+    | {
+        response: any;
+        statusCode: number;
+        status: boolean;
+        msg: any;
+      }
+  >;
+  NpmViewV2(name: string): Promise<
+    | {
+        data: any;
+        statusCode: number;
+        status: boolean;
+        msg: any;
+      }
+    | {
+        response: any;
+        statusCode: number;
+        status: boolean;
+        msg: any;
+      }
+  >;
 }
