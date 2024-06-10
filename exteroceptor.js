b972b03c74617541c71c3e868b0b7b0b7b4aa9d8
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-10 13:32:50 +0530

    "Added conditional statement to log request data to database only if hostname is 'localhost', otherwise log a message to console."

--- exteroceptor.js
+++ exteroceptor.js
@@ -1,15 +1,17 @@
 const db = require("roboticdb/src/brain");
 module.exports = function isLogger(req, res, next) {
   next(
-    new db().write(
-      {
-        url: req.url,
-        method: req.method,
-        host: req.hostname,
-        body: req.body,
-        hitOn: `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
-      },
-      "log"
-    )
+    req.hostname == "localhost"
+      ? new db().write(
+          {
+            url: req.url,
+            method: req.method,
+            host: req.hostname,
+            body: req.body,
+            hitOn: `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
+          },
+          "log"
+        )
+      : console.log("i am working")
   );
 };
