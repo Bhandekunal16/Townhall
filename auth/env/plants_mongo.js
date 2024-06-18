76009ff0687d79912881896f1e257a153003b664
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-18 09:22:47 +0530

    "Replace console.log and console.error with Logger.log and return Error object in connect method"

--- auth/env/plants_mongo.js
+++ auth/env/plants_mongo.js
@@ -55,10 +55,9 @@ class MongoPlants {
   async connect() {
     try {
       await this.client.connect();
-      console.log("Connected to MongoDB");
+      new Logger().log("Connected to MongoDB");
     } catch (error) {
-      console.error("Error connecting to MongoDB", error);
-      throw error;
+      return new Error(error);
     }
   }
 
